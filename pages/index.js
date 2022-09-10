3ef6c57ca18175dcfd045cc72b99048f2e89f681
Drlazinho <lazbonfim@hotmail.com>
2022-09-10 03:02:47 -0300

    update: section team developers

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Layout from "../components/Layout";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
+import Section from "../components/Section";
 
 const Index = () => {
   return (
     <Layout pageTitle="Cripto Space">
       <Header />
       <Hero />
+      <Section/>
       <Footer />
     </Layout>
   );
