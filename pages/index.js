bb733b1beab319517803aed2c15d536af0dfff56
Drlazinho <lazbonfim@hotmail.com>
2022-09-11 03:51:52 -0300

    Cards/Swiper

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import Section from "../components/Section";
+import Cards from "../components/Cards";
 
 const Index = () => {
   return (
     <Layout pageTitle="Cripto Space">
       <Header />
       <Hero />
+      <Cards/>
       <Section/>
       <Footer />
     </Layout>
